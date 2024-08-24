89d6dfd4c87b23ec3dabd852736eaaf717b73483
Brandi <brandi.davis2483@gmail.com>
2024-08-24 01:14:47 -0500

    Form submit created.

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-    res.render("index.ejs");
+let posts = [];
 
+app.get("/", (req, res)=>{
+    res.render("index.ejs", {posts: posts})
+
+});
+
+app.post("/create", (req, res)=>{
+    const newPost ={
+        title: req.body.title,
+        body: req.body.body
+    };
+    var isSubmited = true;
+    console.log(`Test post body: ${req.body.body}`);
+    posts.push(newPost);
+    res.render("index.ejs",{create: isSubmited, posts: posts} )
+    console.log(posts);
+    //res.redirect("/");
 });
 
 app.listen(port, ()=>{
